7a84dd7b08649a5a0930660bd6736983262b9181
GOKUL P <gokulpulikkal@rediff.com>
2017-10-15 11:51:45 +0530

    more constrains added

--- preperation/testing.java
+++ preperation/testing.java
@@ -8,11 +8,39 @@ public class testing{
         map.put("IDN","INDONESIA");
         map.put("IND","INDIA");
         map.put("KER","KERALA");
-        System.out.println("Enter your username: ");
+        System.out.println("ENTER COUNTRY CODE: ");
         Scanner scanner = new Scanner(System.in);
         String username = scanner.nextLine();
-        String us=map.get(username);
-        System.out.println("COUNTRY NAME IS : "+us);
+	int len = username.length();
+	if(len == 3)
+	{
+	        String us=map.get(username);
+		if(us != null)
+		{
+			 System.out.println("COUNTRY NAME IS : "+us);
+		}
+		else
+		{
+			System.out.println("NO SUCH KEY");
+		}
+	}
+	else
+	{
+		int flag=0;
+		Set keys = map.keySet();
+  		for (Iterator i = keys.iterator(); i.hasNext(); ) 
+		{
+       			String key = (String) i.next();
+       			String value = (String) map.get(key);
+       			if(value.equals(username))
+			{
+				System.out.println("CODE IS ="+key);
+				flag=1;	
+			}
+   		}
+		if(flag!=1)
+			System.out.println("NO SUCH COUNTRY");
+	}
         
     }
 }
